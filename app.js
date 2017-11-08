1099122b66fabed823bf25f692541450572bd065
pcbill <pcbill@gmail.com>
2017-11-09 07:50:20 +0800

    intro dateformat, and using flag to figure out records status

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/mydb';
+var dateFormat = require('dateformat');
+
+//const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/mydb';
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/mydb';
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -26,14 +29,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //// helper functions
 
 function findAllPeople(callback) {
-    const sl = 'SELECT * from person';
-    
-    pg.connect(connectionString, function(err, client, done) {
-      client.query(sl, function(err, result) {
+  findPeople('all', callback);
+}
+
+function findPeople(scope, callback) {
+    var sl = 'SELECT * from person where registration_time is not null';
+    if (scope != 'all') {
+        sl += ' AND uid = ' + scope;
+    }
+
+    pg.connect(connectionString, (err, client, done) => {
+      client.query(sl, (err, result) => {
         done();
         if (err) { 
           console.error(err); res.send("Error " + err); 
         } else {
+          result.rows.forEach((it)=>{
+            it.registration_time = dateFormat(it.registration_time, 'yyyyMMdd');
+          });
           callback({results: result.rows});
         }
       });
@@ -42,7 +55,8 @@ function findAllPeople(callback) {
 
 
 function deleteOnePerson(id, callback) {
-    const sl = 'DELETE from person WHERE id = $1';
+    //const sl = 'DELETE from person WHERE id = $1';
+    const sl = 'UPDATE person SET registration_time = null WHERE id = $1';
     const v = [id]
     pg.connect(connectionString, function(err, client, done) {
       client.query(sl, v, function(err, result) {
@@ -64,52 +78,51 @@ app.get('/', (req, res) => {
 
 //// registration
 app.get('/registration', (req, res) => {
-    findAllPeople((list)=>{
+    findAllPeople( (list) => {
       res.render('pages/registration', list);
     });
   }
 );
 
 app.post('/registerSubmit', (req, res) => {
-    var uid = req.body['uid'];
+  var uid = req.body['uid'];
    
-    if (!uid | uid == '') return;
+  if (!uid | uid == '') {
+    res.redirect('/registration');
+    return;
+  }  
 
-    const sl = 'INSERT INTO person(uid) VALUES($1) RETURNING *'
-    const vle = [uid];
-    pg.connect(connectionString, function(err, client, done) {
-      client.query(sl, vle, function(err, result) {
-        done();
-        if (err) { 
-          console.error(err); res.send("Error " + err); 
-        } else { 
-	  findAllPeople((list)=>{
-	    res.render('pages/registration', list);
-	  });
-        }
-      });
+  //const sl = 'INSERT INTO person(uid) VALUES($1) RETURNING *'
+  const sl = 'UPDATE person SET registration_time = NOW() WHERE uid = $1';
+  const vle = [uid];
+  pg.connect(connectionString, function(err, client, done) {
+    client.query(sl, vle, function(err, result) {
+      done();
+      if (err) { 
+        console.error(err); res.send("Error " + err); 
+      } else { 
+        res.redirect('/registration');
+      }
     });
   });
+});
 
 app.get('/deleteRegistration/:id', (req, res) => {
     var id = req.params.id;
     deleteOnePerson(id, ()=>{
-        console.log('success');
+        res.redirect('/registration');
     });
 });
 
 //// game
 app.get('/gameplay', (req, res) => {
     res.render('pages/gameplay');
-  }
-);
+});
 
 app.get('/execute', (req, res) => {
     res.render('pages/execute');
-  }
-);
+});
 
 app.get('/check', (req, res) => {
     res.render('pages/check');
-  }
-);
+});

51082f00c77e7bed1b6d28f7b736de7ca7edb501
pcbill <pcbill@gmail.com>
2017-12-29 22:42:25 +0800

    fix bug

--- app.js
+++ app.js
@@ -354,10 +354,25 @@ app.post('/searchPersonByName', (req, res) => {
     }
   
     personDao.findByName(name, (reObj) => {
-      reObj.msg = req.session['msg'];
-      req.session['msg'] = '';
-      res.render('pages/check', reObj);
-    });
+      var person = reObj.results[0];
+      if (person) {
+        var gameid = person.award_game_id;
+        if (gameid) {
+          gameDao.find(gameid, (reGame) => {
+            person.awardList = reGame.results[0].award_list;
+          });
+        } else {
+            person.awardList = "";
+        }
+        reObj.results[0] = person;
+      } else {
+      }
+
+      setTimeout(function() {
+        reObj.msg = req.session['msg'];
+        req.session['msg'] = '';
+        res.render('pages/check', reObj);
+      }, 1000);
 
 });
 

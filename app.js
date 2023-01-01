6473b26eb963078e369f360d04a14389b96de8e5
Bill SJ Wu <bill_sj_wu@wistron.com>
2023-01-02 06:48:05 +0800

    update basicAuth

--- app.js
+++ app.js
@@ -22,13 +22,35 @@ app.set('port', (process.env.PORT || 5000));
 // });
 
 function basicAuth(req, res, next) {
-    const auth = req.headers.authorization;
-    if (auth === 'password') {
-        next();
-    } else {
-        res.status(401);
-        res.send('Access forbidden');
+
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+        return;
     }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'kdorkwj') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+    }
+
+    // const auth = req.headers.authorization;
+    // if (auth === 'password') {
+    //     next();
+    // } else {
+    //     res.status(401);
+    //     res.send('Access forbidden');
+    // }
 }
 
 app.use(express.static(__dirname + '/public'));

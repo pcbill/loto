393d8b843c7f3989d08baed9c5991fccb2453364
Bill SJ Wu/WHQ/Wistron <Bill_SJ_Wu@wistron.com>
2025-01-14 13:14:40 +0800

    add schema

--- doc/sql/schema/game.sql
+++ doc/sql/schema/game.sql
@@ -1,17 +1,12 @@
-create table game
-(
-    id                integer   default nextval('game_backup_id_seq'::regclass) not null
-        constraint game_backup_pkey
-            primary key,
-    gid               varchar(255)                                              not null,
-    award_list        varchar(1024)                                             not null,
-    participant_count integer,
-    reminder_count    integer,
-    exec_type         integer   default 0,
-    ordered           integer,
-    played_time       timestamp,
-    create_time       timestamp default now()
+drop table game;
+CREATE TABLE game (
+                      id serial primary key,
+                      gid VARCHAR(255) NOT NULL,
+                      award_list VARCHAR(1024) NOT NULL,
+                      participant_count integer,
+                      reminder_count integer,
+                      exec_type integer default 0,               -- 0:normal, 1:drama
+                      ordered integer,
+                      played_time TIMESTAMP,
+                      create_time TIMESTAMP default NOW()
 );
-
-alter table game
-    owner to npust;
